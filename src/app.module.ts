be5782fbd0827b7c489e02f3533e694009b0060b
Kaung Myat Han <arkarhtethan@gmail.com>
2021-07-25 19:18:11 +0630

    typeorm with postgres set up finished

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,27 +1,36 @@
 import { Module } from '@nestjs/common';
+import * as Joi from "joi";
 import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { join } from 'path';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: process.env.NODE_ENV === "dev" ? ".dev.env" : ".test.env"
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('dev', 'prod').required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_DATABASE: Joi.string().required(),
+      }),
+      envFilePath: process.env.NODE_ENV === "dev" ? ".env.dev" : ".env.test",
+      ignoreEnvFile: process.env.NODE_ENV === "prod",
     }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
     }),
     TypeOrmModule.forRoot({
       "type": "postgres",
-      "host": "localhost",
-      "port": 5432,
-      "username": "root",
-      "password": "root",
-      "database": "nuber-eat",
-      "synchronize": true,
+      "host": process.env.DB_HOST,
+      "port": +process.env.DB_PORT,
+      "username": process.env.DB_USERNAME,
+      "password": process.env.DB_PASSWORD,
+      "database": process.env.DB_DATABASE,
+      "synchronize": process.env.NODE_ENV !== "prod",
       "logging": true,
     }),
     RestaurantsModule,
--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,18 +1,31 @@
-import { Field, ObjectType } from "@nestjs/graphql";
+import { Field, Int, ObjectType } from "@nestjs/graphql";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @ObjectType()
+@Entity()
 export class Restaurant {
+    @PrimaryGeneratedColumn()
+    @Field(is => Number)
+    id: number;
 
     @Field(is => String)
+    @Column()
     name: string;
 
     @Field(is => Boolean, { nullable: true })
+    @Column()
     isVegan?: boolean;
 
     @Field(is => String)
+    @Column()
     address: string;
 
     @Field(is => String)
+    @Column()
     ownersName: string;
+
+    @Field(is => String)
+    @Column()
+    categoryName: string;
 
 }
