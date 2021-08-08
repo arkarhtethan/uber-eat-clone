adb4754f7998d21e0c4c7b66c93ae07a515cf5be
Kaung Myat Han <arkarhtethan@gmail.com>
2021-08-08 21:21:54 +0630

    code formatted

--- src/restaurants/restaurants.module.ts
+++ src/restaurants/restaurants.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
-import { Query } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Restaurant } from './entities/restaurant.entity';
 import { RestaurantResolver } from './restaurants.resolver';
+import { RestaurantService } from './restaurants.service';
 
 @Module({
     providers: [
         RestaurantResolver,
-    ]
+        RestaurantService,
+    ],
+    imports: [
+        TypeOrmModule.forFeature([Restaurant])
+    ],
 })
 export class RestaurantsModule {
 }
