43bb49fa60aad8658126e03117478be8fa59fc4f
Kaung Myat Han <arkarhtethan@gmail.com>
2021-08-12 02:15:58 +0630

    export user servicec line added

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtService } from 'src/jwt/jwt.service';
 import { User } from './entities/user.entity';
 import { UsersResolver } from './users.resolver';
 import { UsersService } from './users.service';
@@ -8,10 +9,12 @@ import { UsersService } from './users.service';
 @Module({
     imports: [
         TypeOrmModule.forFeature([User]),
-        ConfigService,
     ],
     providers: [
         UsersResolver,
+        UsersService,
+    ],
+    exports: [
         UsersService,
     ]
 })
