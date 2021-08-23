af112f3ac4637bef26d28e52e375da8a50392923
Kaung Myat Han <arkarhtethan@gmail.com>
2021-08-24 03:30:07 +0630

    delete restaurant feature added

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -4,6 +4,7 @@ import { AuthUser } from "src/auth/auth-user.decorator";
 import { Role } from "src/auth/role.decorator";
 import { User } from "src/users/entities/user.entity";
 import { CreateRestaurantInput, CreateRestaurantOutput } from "./dtos/create-restaurant.dto";
+import { DeleteRestaurantInput } from "./dtos/delete-restaurant.dto";
 import { EditRestaurantInput, EditRestaurantOutput } from "./dtos/edit-restaurant.dto";
 
 import { Restaurant } from "./entities/restaurant.entity";
@@ -30,6 +31,15 @@ export class RestaurantResolver {
         @Args('input') editRestaurantInput: EditRestaurantInput,
     ): Promise<EditRestaurantOutput> {
         return this.restaurantService.editRestaurant(owner, editRestaurantInput)
+    }
+
+    @Mutation(returns => EditRestaurantOutput)
+    @Role(["Owner"])
+    deleteRestaurant (
+        @AuthUser() owner: User,
+        @Args('input') deleteRestaurantInput: DeleteRestaurantInput,
+    ): Promise<EditRestaurantOutput> {
+        return this.restaurantService.deleteRestaurant(owner, deleteRestaurantInput)
     }
 
 }
