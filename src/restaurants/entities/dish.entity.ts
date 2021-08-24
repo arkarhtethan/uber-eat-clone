3ee004c254af614c3ab6291b9e057f86b43cd90f
Kaung Myat Han <arkarhtethan@gmail.com>
2021-08-24 21:26:31 +0630

    constraint updated.dish photo can be nullable.Dishoptions choies and extra can be nullable.

--- src/restaurants/entities/dish.entity.ts
+++ src/restaurants/entities/dish.entity.ts
@@ -10,11 +10,11 @@ class DishOption {
     @Field(type => String)
     name: string;
 
-    @Field(type => [String])
-    choices: string;
+    @Field(type => [String], { nullable: true })
+    choices?: string[];
 
-    @Field(type => Number)
-    extra: number;
+    @Field(type => Number, { nullable: true })
+    extra?: number;
 
 }
 
@@ -33,8 +33,8 @@ export class Dish extends CoreEntity {
     @IsNumber()
     price: number;
 
-    @Field(type => String)
-    @Column()
+    @Field(type => String, { nullable: true })
+    @Column({ nullable: true })
     @IsString()
     photo: string;
 
@@ -55,7 +55,7 @@ export class Dish extends CoreEntity {
     @RelationId((dish: Dish) => dish.restaurant)
     restaurantId: number;
 
-    @Field(type => [DishOption])
-    @Column({ type: 'json' })
+    @Field(type => [DishOption], { nullable: true })
+    @Column({ type: 'json', nullable: true, })
     options: DishOption[]
 }
