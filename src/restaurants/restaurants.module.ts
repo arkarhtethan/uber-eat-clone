70542707188d3c53027cf7dd30e024a331e01169
Kaung Myat Han <arkarhtethan@gmail.com>
2021-08-24 21:08:32 +0630

    Dish repo entity in restaurant module

--- src/restaurants/restaurants.module.ts
+++ src/restaurants/restaurants.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Category } from './entities/category.entity';
+import { Dish } from './entities/dish.entity';
 import { Restaurant } from './entities/restaurant.entity';
 import { CategoryRepository } from './repositories/category.repository';
-import { CategoryResolver, RestaurantResolver } from './restaurants.resolver';
+import { CategoryResolver, DishResolver, RestaurantResolver } from './restaurants.resolver';
 import { RestaurantService } from './restaurants.service';
 
 @Module({
@@ -11,9 +11,10 @@ import { RestaurantService } from './restaurants.service';
         RestaurantResolver,
         RestaurantService,
         CategoryResolver,
+        DishResolver,
     ],
     imports: [
-        TypeOrmModule.forFeature([Restaurant, CategoryRepository])
+        TypeOrmModule.forFeature([Restaurant, CategoryRepository, Dish])
     ],
 })
 export class RestaurantsModule {
