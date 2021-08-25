4bccc047bf4c5b9fcb9b13ee70112f00c8a36262
Kaung Myat Han <arkarhtethan@gmail.com>
2021-08-25 22:23:33 +0630

    OrderItemOption Input/Object Type added and replaced dish option with orderItemOption in OrderItem entity

--- src/orders/entities/order-item.entity.ts
+++ src/orders/entities/order-item.entity.ts
@@ -1,18 +1,28 @@
 import { Field, InputType, ObjectType } from "@nestjs/graphql";
 import { CoreEntity } from "src/common/entities/core.entity";
-import { Dish, DishOption } from "src/restaurants/entities/dish.entity";
-import { Column, Entity, ManyToOne, OneToMany } from "typeorm";
+import { Dish } from "src/restaurants/entities/dish.entity";
+import { Column, Entity, ManyToOne } from "typeorm";
+
+@InputType('OrderItemOptionInputType', { isAbstract: true })
+@ObjectType()
+export class OrderItemOption {
+
+    @Field(type => String)
+    name: string;
+
+    @Field(type => String, { nullable: true })
+    choice?: String;
+}
 
 @InputType('OrderItemInputType', { isAbstract: true })
 @ObjectType()
 @Entity()
 export class OrderItem extends CoreEntity {
-
     @Field(type => [Dish], { nullable: true })
     @ManyToOne(type => Dish, { nullable: true, onDelete: "CASCADE" })
     dish: Dish;
 
-    @Field(type => [DishOption], { nullable: true })
+    @Field(type => [OrderItemOption], { nullable: true })
     @Column({ type: 'json', nullable: true, })
-    options: DishOption[]
+    options: OrderItemOption[]
 }
