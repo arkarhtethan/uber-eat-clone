510329fdc389eec4d5e29213325893e8db9c06e6
Kaung Myat Han <arkarhtethan@gmail.com>
2021-08-25 22:08:35 +0630

    removed unused input

--- src/restaurants/dtos/restaurant.dto.ts
+++ src/restaurants/dtos/restaurant.dto.ts
@@ -1,6 +1,5 @@
 import { Field, InputType, ObjectType } from "@nestjs/graphql";
 import { CoreOutput } from "src/common/dtos/output.dto";
-import { PaginationInput, PaginationOutput } from "src/common/dtos/pagination.dto";
 import { Restaurant } from "../entities/restaurant.entity";
 
 @InputType()
