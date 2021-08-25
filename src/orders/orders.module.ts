613a2a556a302a2241bc0eaf1a29391f72003054
Kaung Myat Han <arkarhtethan@gmail.com>
2021-08-25 10:59:15 +0630

    restaurant repository added in order module

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Restaurant } from 'src/restaurants/entities/restaurant.entity';
 import { Order } from './entities/order.entity';
 import { OrderService } from './order.service';
 import { OrderResolver } from './orders.resolver';
 
 @Module({
     imports: [
-        TypeOrmModule.forFeature([Order])
+        TypeOrmModule.forFeature([Order, Restaurant])
     ],
     providers: [
         OrderService,
