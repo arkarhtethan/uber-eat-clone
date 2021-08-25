64181174b687b2ae2bdec89cf167ba3f0f164dd9
Kaung Myat Han <arkarhtethan@gmail.com>
2021-08-25 22:26:13 +0630

    added entity dependency in orderModule

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,13 +1,20 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Dish } from 'src/restaurants/entities/dish.entity';
 import { Restaurant } from 'src/restaurants/entities/restaurant.entity';
+import { OrderItem } from './entities/order-item.entity';
 import { Order } from './entities/order.entity';
 import { OrderService } from './order.service';
 import { OrderResolver } from './orders.resolver';
 
 @Module({
     imports: [
-        TypeOrmModule.forFeature([Order, Restaurant])
+        TypeOrmModule.forFeature([
+            Order,
+            Restaurant,
+            OrderItem,
+            Dish,
+        ])
     ],
     providers: [
         OrderService,
