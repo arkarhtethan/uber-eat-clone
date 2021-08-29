094063eee90806d736d51c148f9b47bb63987fca
Kaung Myat Han <arkarhtethan@gmail.com>
2021-08-30 03:29:59 +0630

    dependency added for paymentModule

--- src/payments/payments.module.ts
+++ src/payments/payments.module.ts
@@ -1,4 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Restaurant } from 'src/restaurants/entities/restaurant.entity';
+import { RestaurantsModule } from 'src/restaurants/restaurants.module';
+import { Payment } from './entities/payment.entity';
+import { PaymentResolver } from './payment.resolver';
+import { PaymentService } from './payment.service';
 
-@Module({})
-export class PaymentsModule {}
+@Module({
+    imports: [
+        RestaurantsModule,
+        TypeOrmModule.forFeature([Payment, Restaurant])
+    ],
+    providers: [
+        PaymentService,
+        PaymentResolver,
+    ]
+})
+export class PaymentsModule { }
